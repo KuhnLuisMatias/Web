0569f4097493ce385b7a5716846af6a81293e22c
KuhnLuisMatias <matiaskuhn.93@gmail.com>
2023-10-15 13:16:51 -0300

    Not Versioned Files

---
+++ Data/Dto/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Dto
+{
+    public class LoginDto
+    {
+        public string? Nombre { get; set; }
+        public string? Clave { get; set; }
+        public int? Codigo { get; set; }
+        public string? Mail { get; set; }
+    }
+}
